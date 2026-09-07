3def841fa30da08ee3711e9fdedd5c2b0f3c7fed
Abhishek-karma <abhikarma.work@gmail.com>
2026-09-07 09:27:21 +0530

    Release v0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.jarvis.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
