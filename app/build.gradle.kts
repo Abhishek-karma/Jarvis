260f4c0ff8a7fa128cc298ad80e9c06d2630e605
Abhishek-karma <abhikarma.work@gmail.com>
2026-09-09 21:34:00 +0530

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.aistudio.jarvis.abpk"
         minSdk = 29
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.1.6"
+        versionCode = 7
+        versionName = "0.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
